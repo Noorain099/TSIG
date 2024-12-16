b03d44ea7ae381377201793cfdff35e5dad55f7b
Noorain099 <155872157+Noorain099@users.noreply.github.com>
2024-12-16 15:26:24 +0530

    Update odd_even.java

--- odd_even.java
+++ odd_even.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        
         System.out.println("Enter the number : ");
+        int a = sc.nextInt();
         if(a%2==0){
             System.out.println("Even number ");
         }
