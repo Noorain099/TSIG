c84ae1daa7d01afa166a86090227824bb88d5c4f
Noorain099 <155872157+Noorain099@users.noreply.github.com>
2024-12-20 00:07:26 +0530

    Update repeated_element.java

--- repeated_element.java
+++ repeated_element.java
@@ -3,9 +3,9 @@ class MyClass
     public static void main(String args[])
     {  
     
-    int[] arr = {12,3,4,5,6,5,12,2,5,8,7,5,3,5,5};    
+    int[] arr = {12,3,4,5,6,5,12,2,5,8,12,5,5,8,3};    
       System.out.println("Program to find repeating elements in array");
-    
+      int n = arr.length;
       int element = 0;
       int max = 0; 
       for(int i = 0; i < arr.length; i++){
@@ -21,8 +21,13 @@ class MyClass
         }
 
       }
-      System.out.println("the maximum repeating element : "+element+", no of times : "+max);
-       
+      if(max > n / 2)
+      {
+           System.out.println("the maximum repeating element : "+element+", no of times : "+max);
+      }
+      else{
+        System.out.println("No maximum repeating element : ");
+      }
        
     }
 }
